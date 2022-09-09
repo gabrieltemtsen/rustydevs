166b4475264a8f5a13ec7c2bc9479eb0e20484b2
Gabrieltemtsen <gabrieltemtsen@gmail.com>
2022-09-09 16:11:54 +0100

    fix:createTicketFunction

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -5,34 +5,30 @@ contract AvaxLottery{
     address payable public owner ;
     uint public prize_pool;
 
-    // Enum to track ticket progress
     enum TicketStatus {
         Pending,
         InProgress,
         Ended
     }
     
-    //initialize enum
     TicketStatus public ticket_status ;
 
-    // Info about ticket
     struct TicketInfo {
         string title;
-        uint[6] public generated_numbers; // array of random generated numbers
+        uint[6] generated_numbers; // array of random generated numbers
     }
 
-    //initialize struct
     TicketInfo[] public ticket_info;
 
-    // struct User {
-    //     uint id;
-    //     uint[6] ticket;
-    //     uint ticket_type;
-    // }
+    struct User {
+        uint id;
+        uint[6] ticket;
+        uint ticket_type;
+    }
 
     constructor() {
         // Set the transaction sender as the owner of the contract.
-        owner = payable(msg.sender());
+        owner = payable(msg.sender);
     }
 
     modifier onlyOwner() {
@@ -55,12 +51,12 @@ contract AvaxLottery{
         
     }
 
-    // Function to create ticket by the owner of the contract
-    function create_ticket (string memory _text, uint[] memory _numbers) public onlyOwner returns (TicketInfo){
-        ticket_info.push(TicketInfo(_numbers, _text));
+     // Function to create ticket by the owner of the contract
+    function create_ticket (string memory _text, uint[6] memory _numbers) public onlyOwner{
+        ticket_info.push(TicketInfo( _text, _numbers));
     }
 
-    function get_generated_numbers() public view returns (uint[] memory) {
-        return generated_numbers;
-    }
+    // function get_generated_numbers() public view returns (uint[6]) {
+    //     return ticket_info.generated_numbers;
+    // }
 }
