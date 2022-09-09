48bcd0fce69bb8315783187b4589fba68b810e59
jerrymusaga <musagatechnology@gmail.com>
2022-09-09 12:38:21 +0100

    chore:initiating function methods

--- contract/Token.sol
+++ contract/Token.sol
@@ -2,7 +2,18 @@
 pragma solidity ^0.8.3;
 
 contract AvaxLottery{
+    address payable public owner ;
     uint public prize_pool;
+
+    constructor() {
+        // Set the transaction sender as the owner of the contract.
+        owner = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner of Contract");
+        _;
+    }
 
     // Function to get the current prize pool
     function get_pool_prize() public view returns (uint) {
@@ -17,5 +28,12 @@ contract AvaxLottery{
     // Function to transfer prize base on winning tickets
     function transfer_prize() public {
         
+    }
+
+    // array of random generated numbers
+    uint[] public generated_numbers;
+
+    function get_generated_numbers() public view returns (uint[] memory) {
+        return generated_numbers;
     }
 }
