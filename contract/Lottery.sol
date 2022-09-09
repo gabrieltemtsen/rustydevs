63ed8f4da65138820196f5cd06299b51639d3044
jerrymusaga <musagatechnology@gmail.com>
2022-09-09 14:46:13 +0100

    chore:adding comments to function

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -5,26 +5,30 @@ contract AvaxLottery{
     address payable public owner ;
     uint public prize_pool;
 
+    // Enum to track ticket progress
     enum TicketStatus {
         Pending,
         InProgress,
         Ended
     }
     
+    //initialize enum
     TicketStatus public ticket_status ;
 
+    // Info about ticket
     struct TicketInfo {
         string title;
         uint[6] public generated_numbers; // array of random generated numbers
     }
 
-    TicketInfo[] public numbers;
+    //initialize struct
+    TicketInfo[] public ticket_info;
 
-    struct User {
-        uint id;
-        uint[6] ticket;
-        uint ticket_type;
-    }
+    // struct User {
+    //     uint id;
+    //     uint[6] ticket;
+    //     uint ticket_type;
+    // }
 
     constructor() {
         // Set the transaction sender as the owner of the contract.
@@ -52,8 +56,8 @@ contract AvaxLottery{
     }
 
     // Function to create ticket by the owner of the contract
-    function create_ticket (string memory _text, uint[] memory _numbers) public onlyOwner() returns (TicketInfo){
-        numbers.push(TicketInfo(_numbers, _text));
+    function create_ticket (string memory _text, uint[] memory _numbers) public onlyOwner returns (TicketInfo){
+        ticket_info.push(TicketInfo(_numbers, _text));
     }
 
     function get_generated_numbers() public view returns (uint[] memory) {
