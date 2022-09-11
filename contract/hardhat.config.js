290a698539b8a67b37fbf904b5ba902cb088ae80
jerrymusaga <musagatechnology@gmail.com>
2022-09-11 11:31:16 +0100

    chore: deleting contract folder

--- contract/hardhat.config.js
+++
@@ -1,6 +0,0 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-// require("@nomicfoundation/hardhat-chai-matchers");
-// require("@nomiclabs/hardhat-ethers");
-module.exports = {
-  solidity: "0.8.9",
-};
